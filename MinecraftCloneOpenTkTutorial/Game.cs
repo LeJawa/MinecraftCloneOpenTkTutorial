a5294e4f30fddeb5f8618f6cdfe297becc004516
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-05-04 12:23:59 +0200

    How to Code Minecraft without a Game Engine #2

---
+++ MinecraftCloneOpenTkTutorial/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace MinecraftCloneOpenTkTutorial
+{
+    internal class Game : GameWindow
+    {
+        // CONSTANTS
+        private static int SCREENWIDTH;
+        private static int SCREENHEIGHT;
+
+        public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            SCREENWIDTH = width;
+            SCREENHEIGHT = height;
+
+            // Center the window on monitor
+            CenterWindow(new Vector2i(SCREENWIDTH, SCREENHEIGHT));
+        }
+    }
+}
